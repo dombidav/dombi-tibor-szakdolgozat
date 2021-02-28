0fd4eb35d4fff64e344a74b1ce3426b73213c049
Dombi Tibor <dombidav@gmail.com>
2021-02-28 22:05:34 +0100

    Created User Controller

--- src/web-service/app/Http/Controllers/UserController.php
+++ src/web-service/app/Http/Controllers/UserController.php
@@ -2,34 +2,62 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\UserRequest;
+use App\Http\Resources\UserResource;
 use App\Models\User;
-use Illuminate\Http\Request;
+use Exception;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+use Symfony\Component\HttpFoundation\Response as ResponseCode;
 
 class UserController extends Controller
 {
 
     public function index()
     {
-        return User::all();
+        return UserResource::collection(User::all());
     }
 
-    public function store(Request $request)
+    public function store(UserRequest $request)
     {
-
+        $validated = $request->except('password_confirmation');
+        $validated['password'] = Hash::make($validated['password']);
+        $user = User::create($validated);
+        return response(UserResource::make($user))->setStatusCode(201);
     }
 
     public function show(User $user)
     {
-        //
+        return UserResource::make($user);
     }
 
-    public function update(Request $request, User $user)
+    /**
+     * @param UserRequest $request
+     * @param User $user
+     * @return JsonResponse|object
+     */
+    public function update(UserRequest $request, User $user)
     {
-        //
+        if($request->has('password') && $user !== Auth::user()) {
+            return response()->json(['message' => 'You can not change the password of another user'])->setStatusCode(ResponseCode::HTTP_FORBIDDEN);
+        }
+        $user->update($request->validated());
+        $user->save();
+        return response()->setStatusCode(ResponseCode::HTTP_NO_CONTENT);
     }
 
-    public function destroy(User $user)
+    /**
+     * @param UserRequest $request
+     * @param User $user
+     * @return UserResource
+     * @throws Exception
+     * @noinspection PhpUnusedParameterInspection
+     */
+    public function destroy(UserRequest $request, User $user)
     {
-        //
+        $old = UserResource::make($user);
+        $user->delete();
+        return $old;
     }
 }
