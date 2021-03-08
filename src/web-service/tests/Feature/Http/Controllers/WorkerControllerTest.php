304f6e7fcbdce62dddc358c1596ba9a1711c8ecd
Dombi Tibor <dombidav@gmail.com>
2021-03-08 18:27:21 +0100

    Generated Worker files

---
+++ src/web-service/tests/Feature/Http/Controllers/WorkerControllerTest.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Http\Controllers;
+
+use Illuminate\Support\Facades\Artisan;
+use Tests\TestCase;
+use Symfony\Component\HttpFoundation\Response as ResponseCode;
+
+class WorkerControllerTest extends TestCase
+{
+
+}
