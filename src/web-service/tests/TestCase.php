1597fd98f723e579dcfa597d5097b12b00483eed
Dombi Tibor <dombidav@gmail.com>
2021-03-11 17:36:37 +0100

    Moved test helper methods to base class

--- src/web-service/tests/TestCase.php
+++ src/web-service/tests/TestCase.php
@@ -2,12 +2,16 @@
 
 namespace Tests;
 
+use App\Models\User;
 use Exception;
 use Faker\Factory;
 use Faker\Generator;
+use Illuminate\Database\Eloquent\Model;
 use Illuminate\Foundation\Testing\DatabaseMigrations;
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
+use Illuminate\Http\Resources\Json\JsonResource;
 use Illuminate\Support\Facades\Artisan;
+use Symfony\Component\HttpFoundation\Response as ResponseCode;
 
 abstract class TestCase extends BaseTestCase {
 
@@ -16,12 +20,20 @@ abstract class TestCase extends BaseTestCase {
 
     private Generator $faker;
 
-    public function setUp()
-    : void {
+    /** @var User[] $users */
+    protected array $users;
 
+    public function setUp(): void
+    {
         parent::setUp();
         $this->faker = Factory::create();
         Artisan::call('migrate:refresh');
+        Artisan::call('db:seed');
+        $this->users = [
+            'admin' => User::where( 'name', 'Administrator')->first(),
+            'supervisor' => User::where( 'name', 'Test Supervisor')->first(),
+            'guard' => User::where( 'name', 'Test Security Guard')->first()
+        ];
     }
 
     public function __get($key) {
@@ -29,5 +41,76 @@ abstract class TestCase extends BaseTestCase {
         if ($key === 'faker')
             return $this->faker;
         throw new Exception('Unknown Key Requested');
+    }
+
+    /**
+     * @param string $routingTo Route to GET from
+     * @param JsonResource $iShouldGet Expected JSON
+     * @param array $sending Request Body
+     * @param int $resultCountWillBe Count of resulting array if applicable. -1 to ignore.
+     */
+    public function assertModel(string $routingTo, JsonResource $iShouldGet, array $sending = [], int $resultCountWillBe = -1): void
+    {
+        foreach ($this->users as $user) {
+            $response = $this->actingAs($user)->getJson(route($routingTo, $sending));
+            $response->assertJsonFragment([$iShouldGet->jsonSerialize()]);
+            if($resultCountWillBe > -1) {
+                self::assertCount($resultCountWillBe, $response->json('data'));
+            }
+        }
+    }
+
+    public function assertPermissionCanCreate(string $actingAs, string $canCreate, array $sending, array $expecting): void
+    {
+        $response = $this->actingAs($this->users[$actingAs])->post(route("$canCreate.store"), $sending);
+        $response->assertStatus(ResponseCode::HTTP_CREATED);
+        $response->assertJsonFragment($expecting);
+    }
+
+    public function assertPermissionCanNotCreate(string $actingAs, string $canNotCreate, array $sending): \Illuminate\Testing\TestResponse
+    {
+        $response = $this->actingAs($this->users[$actingAs])->post(route("$canNotCreate.store"), $sending);
+        $response->assertStatus(ResponseCode::HTTP_FORBIDDEN);
+        return $response;
+    }
+
+    public function assertPermissionCanUpdate(string $actingAs, string $canUpdate, array $sending, Model $expecting): void
+    {
+        $response = $this->actingAs($this->users[$actingAs])->put(route("$canUpdate.update", $expecting->id), $sending);
+        $response->assertStatus(ResponseCode::HTTP_NO_CONTENT);
+    }
+    public function assertPermissionCanNotUpdate(string $actingAs, string $canNotUpdate, array $sending, Model $expecting): void
+    {
+        $response = $this->actingAs($this->users[$actingAs])->put(route("$canNotUpdate.update", $expecting->id), $sending);
+        $response->assertStatus(ResponseCode::HTTP_FORBIDDEN);
+    }
+
+    /**
+     * @param string $actingAs
+     * @param string $canDelete
+     * @param $target
+     * @return \Illuminate\Testing\TestResponse
+     */
+    public function assertPermissionCanDelete(
+        string $actingAs,
+        string $canDelete,
+        $target
+    ): \Illuminate\Testing\TestResponse {
+        $response = $this->actingAs($this->users[$actingAs])->delete(route("$canDelete.destroy", $target));
+        $response->assertStatus(ResponseCode::HTTP_NO_CONTENT);
+        return $response;
+    }
+
+    /**
+     * @param string $actingAs
+     * @param string $canNotDelete
+     * @param $target
+     * @return \Illuminate\Testing\TestResponse
+     */
+    public function assertPermissionCanNotDelete(string $actingAs, string $canNotDelete, $target): \Illuminate\Testing\TestResponse
+    {
+        $response = $this->actingAs($this->users[$actingAs])->delete(route("$canNotDelete.destroy", $target));
+        $response->assertStatus(ResponseCode::HTTP_FORBIDDEN);
+        return $response;
     }
 }
