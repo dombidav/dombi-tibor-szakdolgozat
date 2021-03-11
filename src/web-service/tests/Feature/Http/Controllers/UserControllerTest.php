e13047e65ee3145fce3c55402bba2eab1c331008
Dombi Tibor <dombidav@gmail.com>
2021-03-11 13:32:12 +0100

    Created reusable functions for testing

--- src/web-service/tests/Feature/Http/Controllers/UserControllerTest.php
+++ src/web-service/tests/Feature/Http/Controllers/UserControllerTest.php
@@ -4,18 +4,20 @@ namespace Http\Controllers;
 
 use App\Http\Resources\UserResource;
 use App\Models\User;
+use Database\Factories\UserFactory;
+use Illuminate\Database\Eloquent\Model;
 use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Http\Resources\Json\JsonResource;
 use Illuminate\Support\Facades\Artisan;
 use Illuminate\Support\Facades\Hash;
 use Tests\TestCase;
 use Symfony\Component\HttpFoundation\Response as ResponseCode;
-
 class UserControllerTest extends TestCase
 {
     use RefreshDatabase;
 
     /** @var User[] $users */
-    private array $users;
+    protected array $users;
 
     public function setUp(): void
     {
@@ -28,128 +30,63 @@ class UserControllerTest extends TestCase
         ];
     }
 
-    public function testUserIndex()
-    {
-        foreach ($this->users as $user){
-            $response = $this->actingAs($user)->getJson(route('user.index'));
-            $response->assertJsonStructure([
-                'data' => [
-                    '*' => [
-                        'id',
-                        'name',
-                        'email',
-                        'email_verified_at',
-                        'created_at',
-                        'updated_at',
-                        'roles' => [
-                            '*' => []
-                        ],
-                        'abilities' => [
-                            '*' => []
-                        ],
-                        'forbidden' => [
-                            '*' => []
-                        ]
-                    ]
-                ]
-            ]);
-        }
+    public function testUserIndex(){
+        $this->assertModel('user.index', UserResource::make(User::first()), [], User::count());
     }
 
     public function testUserShow(){
-        $expected = User::first();
-
-        foreach ($this->users as $user){
-            $response = $this->actingAs($user)->getJson(route('user.show', ['user' => $expected->id]));
-            $response->assertJsonStructure([
-                'data' => [
-                    'id',
-                    'name',
-                    'email',
-                    'email_verified_at',
-                    'created_at',
-                    'updated_at',
-                    'roles' => [
-                        '*' => []
-                    ],
-                    'abilities' => [
-                        '*' => []
-                    ],
-                    'forbidden' => [
-                        '*' => []
-                    ]
-                ]
-            ]);
-            $response->assertExactJson(['data' => UserResource::make($expected)->jsonSerialize()]);
-        }
+        $this->assertModel('user.show', UserResource::make(User::first()), ['user' => User::first()->id]);
     }
 
     public function testAdminCanCreateUsers(){
-        $password = Hash::make('secret');
-        $response = $this->actingAs($this->users['admin'])->post(route('user.store'), [
+        $requestModel = [
             'name' => 'Test User',
             'email' => 'test@test.test',
-            'password' => $password,
-            'password_confirmation' => $password
-        ]);
-
-        $response->assertJsonFragment([
+            'password' => 'secret',
+            'password_confirmation' => 'secret'
+        ];
+        $expectedData = [
             'name' => 'Test User',
             'email' => 'test@test.test'
-        ]);
+        ];
+
+        $this->assertPermissionCanCreate('admin', 'user', $requestModel, $expectedData);
     }
 
     public function testOthersCanNotCreateUsers(){
-        $password = Hash::make('secret');
-        $user = [
+        $requestModel = [
             'name' => 'Test User',
             'email' => 'test@test.test',
-            'password' => $password,
-            'password_confirmation' => $password
+            'password' => 'secret',
+            'password_confirmation' => 'secret'
         ];
-        $response = $this->actingAs($this->users['supervisor'])->post(route('user.store'), $user);
-        $response->assertStatus(ResponseCode::HTTP_FORBIDDEN);
 
-        $response = $this->actingAs($this->users['guard'])->post(route('user.store'), $user);
-        $response->assertStatus(ResponseCode::HTTP_FORBIDDEN);
+        $this->assertPermissionCanNotCreate('supervisor', 'user', $requestModel);
+        $this->assertPermissionCanNotCreate('guard', 'user', $requestModel);
     }
 
     public function testAdminCanUpdateUser(){
-        $user = [
-            'name' => 'Updated User'
-        ];
-        $expected = User::latest()->first();
-        $response = $this->actingAs($this->users['admin'])->put(route('user.update', $expected->id), $user);
-        $response->assertStatus(ResponseCode::HTTP_NO_CONTENT);
+        $this->assertPermissionCanUpdate('admin', 'user', ['name' => 'Updated User'], User::latest()->first());
     }
 
     public function testAdminCanOnlyUpdateOwnPassword(){
-        $data = [
+        $requestModel = [
             'password' => 'updated_secret',
             'password_confirmation' => 'updated_secret',
             'old_password' => 'secret'
         ];
-        $response = $this->actingAs($this->users['admin'])->put(route('user.update', $this->users['admin']->id), $data);
-        $response->assertStatus(ResponseCode::HTTP_NO_CONTENT);
-
-        $response = $this->actingAs($this->users['admin'])->put(route('user.update', $this->users['supervisor']->id), $data);
-        $response->assertStatus(ResponseCode::HTTP_FORBIDDEN);
+        $this->assertPermissionCanUpdate('admin', 'user', $requestModel, $this->users['admin']);
+        $this->assertPermissionCanNotUpdate('admin', 'user', $requestModel, $this->users['supervisor']);
     }
 
     public function testOthersCanNotUpdateUsers(){
-        $user = [
-            'name' => 'Updated User'
-        ];
-        $expected = User::latest()->first();
-        $response = $this->actingAs($this->users['supervisor'])->put(route('user.update', $expected->id), $user);
-        $response->assertStatus(ResponseCode::HTTP_FORBIDDEN);
-
-        $response = $this->actingAs($this->users['guard'])->put(route('user.update', $expected->id), $user);
-        $response->assertStatus(ResponseCode::HTTP_FORBIDDEN);
+        $this->assertPermissionCanNotUpdate('supervisor', 'user',  ['name' => 'Updated User'], User::latest()->first());
+        $this->assertPermissionCanNotUpdate('guard', 'user', ['name' => 'Updated User'], User::latest()->first());
     }
 
     public function testAdminCanDeleteUsers(){
-        $response = $this->actingAs($this->users['admin'])->delete(route('user.destroy', $this->users['supervisor']->id));
+        $model = 'user';
+        $response = $this->actingAs($this->users['admin'])->delete(route("$model.destroy", $this->users['supervisor']->id));
         $response->assertStatus(ResponseCode::HTTP_NO_CONTENT);
 
         $response = $this->actingAs($this->users['admin'])->delete(route('user.destroy', $this->users['admin']->id));
@@ -161,6 +98,48 @@ class UserControllerTest extends TestCase
         $response->assertStatus(ResponseCode::HTTP_FORBIDDEN);
 
         $response = $this->actingAs($this->users['supervisor'])->delete(route('user.destroy', $this->users['supervisor']->id));
+        $response->assertStatus(ResponseCode::HTTP_FORBIDDEN);
+    }
+
+    /**
+     * @param string $routingTo Route to GET from
+     * @param JsonResource $iShouldGet Expected JSON
+     * @param array $sending Request Body
+     * @param int $resultCountWillBe Count of resulting array if applicable. -1 to ignore.
+     */
+    public function assertModel(string $routingTo, JsonResource $iShouldGet, array $sending = [], int $resultCountWillBe = -1): void
+    {
+        foreach ($this->users as $user) {
+            $response = $this->actingAs($user)->getJson(route($routingTo, $sending));
+            $response->assertJsonFragment([$iShouldGet->jsonSerialize()]);
+            if($resultCountWillBe > -1) {
+                self::assertCount($resultCountWillBe, $response->json('data'));
+            }
+        }
+    }
+
+    public function assertPermissionCanCreate(string $actingAs, string $canCreate, array $sending, array $expecting): void
+    {
+        $response = $this->actingAs($this->users[$actingAs])->post(route("$canCreate.store"), $sending);
+        $response->assertStatus(ResponseCode::HTTP_CREATED);
+        $response->assertJsonFragment($expecting);
+    }
+
+    public function assertPermissionCanNotCreate(string $actingAs, string $canNotCreate, array $sending): \Illuminate\Testing\TestResponse
+    {
+        $response = $this->actingAs($this->users[$actingAs])->post(route("$canNotCreate.store"), $sending);
+        $response->assertStatus(ResponseCode::HTTP_FORBIDDEN);
+        return $response;
+    }
+
+    public function assertPermissionCanUpdate(string $actingAs, string $canUpdate, array $sending, Model $expecting): void
+    {
+        $response = $this->actingAs($this->users[$actingAs])->put(route("$canUpdate.update", $expecting->id), $sending);
+        $response->assertStatus(ResponseCode::HTTP_NO_CONTENT);
+    }
+    public function assertPermissionCanNotUpdate(string $actingAs, string $canNotUpdate, array $sending, Model $expecting): void
+    {
+        $response = $this->actingAs($this->users[$actingAs])->put(route("$canNotUpdate.update", $expecting->id), $sending);
         $response->assertStatus(ResponseCode::HTTP_FORBIDDEN);
     }
 }
