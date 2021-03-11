72b7d8acde6e65ec7abb53f716fb8b89cc4fa2e0
Dombi Tibor <dombidav@gmail.com>
2021-03-11 19:05:54 +0100

    Created APIResource trait

---
+++ src/web-service/app/Traits/ApiResource.php
@@ -0,0 +1,29 @@
+<?php
+
+
+namespace App\Traits;
+
+
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\Model;
+
+/**
+ * @method static Model|null first()
+ * @method static int count()
+ * @method static Builder latest()
+ * @method static Builder inRandomOrder()
+ */
+
+trait ApiResource
+{
+
+    public static function latestOne(): Model
+    {
+        return self::latest()->first();
+    }
+
+    public static function random(): Model
+    {
+        return self::inRandomOrder()->first();
+    }
+}
