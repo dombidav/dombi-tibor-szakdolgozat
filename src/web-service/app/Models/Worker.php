45ea0a3d880ad717e1b822baa6c12e353cba8afc
Dombi Tibor <dombidav@gmail.com>
2021-03-11 19:06:45 +0100

    Added ApiResource trait for Models

--- src/web-service/app/Models/Worker.php
+++ src/web-service/app/Models/Worker.php
@@ -2,10 +2,15 @@
 
 namespace App\Models;
 
+use App\Traits\ApiResource;
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Worker extends Model
 {
     use HasFactory;
+    use ApiResource;
+
+    
 }
