4406686e8a14461b1d7240f6ffd73aecdc4af2ca
Dombi Tibor <dombidav@gmail.com>
2021-03-27 20:38:38 +0100

    Added test: Detach Worker from group

--- src/web-service/tests/Feature/Http/Controllers/GroupControllerTest.php
+++ src/web-service/tests/Feature/Http/Controllers/GroupControllerTest.php
@@ -43,12 +43,12 @@ class GroupControllerTest extends TestCase
     public function testGroupAttach(): void
     {
 
-        /** @var Worker $worker */
-        $worker = Worker::random();
-        $numWorkers = $worker->groups()->count();
         /** @var Group $group */
         $group = Group::random();
         $numGroup = $group->workers()->count();
+        /** @var Worker $worker */
+        $worker = Worker::random();
+        $numWorkers = $worker->groups()->count();
         $response = $this->actingAs($this->users['admin'])->post(route('group.attach'), ['group_id' => $group->id, 'worker_id' => $worker->id], ['Accept' => 'application/json']);
         $response->assertStatus(ResponseCode::HTTP_NO_CONTENT);
 
@@ -57,5 +57,23 @@ class GroupControllerTest extends TestCase
 
         self::assertEquals($numWorkers + 1, $worker->groups()->count());
         self::assertEquals($numGroup + 1, $group->workers()->count());
+    }
+
+    public function testGroupDetach(): void
+    {
+        /** @var Group $group */
+        $group = Group::random();
+        $numGroup = $group->workers()->count();
+        /** @var Worker $worker */
+        $worker = $group->workers()->inRandomOrder()->first();
+        $numWorkers = $worker->groups()->count();
+        $response = $this->actingAs($this->users['admin'])->delete(route('group.detach'), ['group_id' => $group->id, 'worker_id' => $worker->id], ['Accept' => 'application/json']);
+        $response->assertStatus(ResponseCode::HTTP_NO_CONTENT);
+
+        $worker->refresh();
+        $group->refresh();
+
+        self::assertEquals($numWorkers - 1, $worker->groups()->count());
+        self::assertEquals($numGroup - 1, $group->workers()->count());
     }
 }
