69e985c288ae991f04238d4f58c166f32b1946e8
Dombi Tibor <dombidav@gmail.com>
2021-04-09 14:19:40 +0200

    Bugfix: Added auth guard to resource routes

--- src/web-service/routes/api.php
+++ src/web-service/routes/api.php
@@ -23,23 +23,26 @@ Route::group([
     Route::post('/signout', [JwtAuthController::class, 'signout']);
 });
 
-Route::group(['middleware' => 'api'], function ($router){
-    Route::post('/team-control', [TeamController::class, 'attach'])->name('team.attach');
-    Route::delete('/team-control', [TeamController::class, 'detach'])->name('team.detach');
-    Route::post('/lock-control', [LockGroupController::class, 'attach'])->name('lock_group.attach');
-    Route::delete('/lock-control', [LockGroupController::class, 'detach'])->name('lock_group.detach');
+Route::group(['middleware' => 'auth'],   function ($router){
+    //Non-Resource type routes
+    Route::group(['middleware' => 'api'], function ($router){
+        Route::post('/team-control', [TeamController::class, 'attach'])->name('team.attach');
+        Route::delete('/team-control', [TeamController::class, 'detach'])->name('team.detach');
+        Route::post('/lock-control', [LockGroupController::class, 'attach'])->name('lock_group.attach');
+        Route::delete('/lock-control', [LockGroupController::class, 'detach'])->name('lock_group.detach');
 
-    Route::post('/rule-control', [AccessRuleController::class, 'attach'])->name('rule.attach');
-    Route::delete('/rule-control', [AccessRuleController::class, 'detach'])->name('rule.detach');
+        Route::post('/rule-control', [AccessRuleController::class, 'attach'])->name('rule.attach');
+        Route::delete('/rule-control', [AccessRuleController::class, 'detach'])->name('rule.detach');
 
-    Route::put('/access/{device_key}', [AccessController::class, 'enter'])->name('access.enter');
-});
+        Route::put('/access/{device_key}', [AccessController::class, 'enter'])->name('access.enter');
+    });
 
 //RESOURCES
-Route::apiResource('user', UserController::class);
-Route::apiResource('worker', WorkerController::class);
-Route::apiResource('lock', LockController::class);
-Route::apiResource('team', TeamController::class);
-Route::apiResource('access_rule', AccessRuleController::class);
-Route::apiResource('lock_group', LockGroupController::class);
-Route::apiResource('log', LogController::class)->except(['store', 'update', 'destroy']);
+    Route::apiResource('user', UserController::class);
+    Route::apiResource('worker', WorkerController::class);
+    Route::apiResource('lock', LockController::class);
+    Route::apiResource('team', TeamController::class);
+    Route::apiResource('access_rule', AccessRuleController::class);
+    Route::apiResource('lock_group', LockGroupController::class);
+    Route::apiResource('log', LogController::class)->except(['store', 'update', 'destroy']);
+});
